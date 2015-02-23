c821963e4687fc6b013e7e6a935cbc5a798d8ce3
Damian Hickey <dhickey@gmail.com>
2015-02-23 15:17:58 +0100

    Seperate projecton handlers, modules etc.

---
+++ src/Cedar.Tests/Projections/ProjectionTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Cedar.Projections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ProjectionTests
+    {
+        [Fact]
+        public void Can_resolve_handlers()
+        {
+            var handlerResolver = new ProjectionHandlerResolver(new SampeProjectionModule(new List<object>()));
+            var handlers = handlerResolver.ResolveAll<SampleEvent>().ToArray();
+
+            handlers.Length.Should().Be(1);
+        }
+
+        [Fact]
+        public void Can_invoke_handlers()
+        {
+            List<object> projectedEvents = new List<object>();
+            var handlerResolver = new ProjectionHandlerResolver(new SampeProjectionModule(projectedEvents));
+            var handlers = handlerResolver.ResolveAll<SampleEvent>().ToArray();
+
+            var projectionEvent = new ProjectionEvent<SampleEvent>("streamid", Guid.NewGuid(), 1, DateTimeOffset.UtcNow, null, new SampleEvent());
+
+            foreach(var handler in handlers)
+            {
+                handler(projectionEvent, CancellationToken.None);
+            }
+        }
+
+        private class SampeProjectionModule : ProjectionHandlerModule
+        {
+            public SampeProjectionModule(List<object> projectedEvents)
+            {
+                For<SampleEvent>()
+                    .Pipe(next => next)
+                    .Handle(projectedEvents.Add);
+            }
+        }
+
+        private class SampleEvent { }
+    }
+}
---
+++ src/Cedar/Projections/ProjectionEvent.cs
@@ -0,0 +1,62 @@
+﻿namespace Cedar.Projections
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class ProjectionEvent<T>
+        where T : class
+    {
+        private readonly string _streamId;
+        private readonly Guid _eventId;
+        private readonly int _sequence;
+        private readonly DateTimeOffset _timeStampe;
+        private readonly IDictionary<string, object> _headers;
+        private readonly T _event;
+
+        public ProjectionEvent(
+            string streamId,
+            Guid eventId,
+            int sequence,
+            DateTimeOffset timeStampe,
+            IDictionary<string, object> headers,
+            T @event)
+        {
+            _streamId = streamId;
+            _eventId = eventId;
+            _sequence = sequence;
+            _timeStampe = timeStampe;
+            _headers = headers;
+            _event = @event;
+        }
+
+        public string StreamId
+        {
+            get { return _streamId; }
+        }
+
+        public Guid EventId
+        {
+            get { return _eventId; }
+        }
+
+        public int Sequence
+        {
+            get { return _sequence; }
+        }
+
+        public DateTimeOffset TimeStampe
+        {
+            get { return _timeStampe; }
+        }
+
+        public IDictionary<string, object> Headers
+        {
+            get { return _headers; }
+        }
+
+        public T Event
+        {
+            get { return _event; }
+        }
+    }
+}
