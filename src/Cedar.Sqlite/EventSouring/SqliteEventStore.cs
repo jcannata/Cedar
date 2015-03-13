5cb3bb87f01c34f9f1dee1f2f4ed62e031b348dc
Damian Hickey <dhickey@gmail.com>
2015-03-13 01:27:42 +0100

    Almost there with first Sqlite tests.

--- src/Cedar.Sqlite/EventSouring/SqliteEventStore.cs
+++ src/Cedar.Sqlite/EventSouring/SqliteEventStore.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
     using System.Threading.Tasks;
     using Cedar.EventSourcing;
     using SQLite.Net;
@@ -10,20 +12,60 @@
 
     public class SqliteEventStore : IEventStore
     {
+        private readonly GetUtcNow _getUtcNow;
         private readonly Func<SQLiteConnectionWithLock> _getConnection;
         private readonly SQLiteConnectionPool _connectionPool;
         private string _databasePath;
 
-        public SqliteEventStore(ISQLitePlatform sqLitePlatform, string databasePath)
+        public SqliteEventStore(ISQLitePlatform sqLitePlatform, string databasePath, GetUtcNow getUtcNow = null)
         {
+            _getUtcNow = getUtcNow ?? SystemClock.GetUtcNow;
             _connectionPool = new SQLiteConnectionPool(sqLitePlatform);
             var connectionString = new SQLiteConnectionString(databasePath, false);
             _getConnection = () => _connectionPool.GetConnection(connectionString);
+
         }
 
         public Task AppendToStream(string streamId, int expectedVersion, IEnumerable<NewSteamEvent> events)
         {
-            throw new System.NotImplementedException();
+            var connection = _getConnection();
+            connection.BeginTransaction();
+
+            //TODO Idempotency check
+            /*var sequence = connection.Table<Event>()
+                .Where(e => e.BucketId == "default" && e.StreamId == streamId)
+                .OrderByDescending(e => e.SequenceNumber)
+                .Select(e => e.SequenceNumber)
+                .Take(1)
+                .ToList()
+                .FirstOrDefault();
+
+            if(sequence != expectedVersion)
+            {
+                throw new Exception();
+            }*/
+
+            var sequence = 0;
+            var eventsToInsert = events.Select(e => new Event
+            {
+                Body = e.Body,
+                BucketId = "default",
+                EventId = e.EventId,
+                Headers = null,
+                IsDeleted = false,
+                OriginalStreamId = streamId,
+                SequenceNumber = sequence++,
+                Stamp = _getUtcNow(),
+                StreamId = streamId
+            });
+
+            foreach (var eventToInsert in eventsToInsert)
+            {
+                connection.Insert(eventToInsert);
+            }
+            connection.Commit();
+
+            return Task.FromResult(0);
         }
 
         public Task DeleteStream(string streamId, int expectedVersion = ExpectedVersion.Any, bool hardDelete = true)
@@ -38,7 +80,25 @@
 
         public Task<IStreamEventsPage> ReadStream(string streamId, int start, int count, ReadDirection direction = ReadDirection.Forward)
         {
-            throw new System.NotImplementedException();
+            var connection = _getConnection();
+
+            Event[] results = connection.Table<Event>().Where(e => e.BucketId == "default" && e.StreamId == streamId)
+                .OrderBy(e => e.SequenceNumber)
+                .Skip(start)
+                .Take(count)
+                .ToArray();
+
+            IStreamEventsPage streamEventsPage = new StreamEventsPage(
+                streamId,
+                PageReadStatus.Success,
+                direction,
+                results[0].SequenceNumber,
+                results[results.Length - 1].SequenceNumber,
+                results[results.Length - 1].SequenceNumber + 1,
+                true,
+                results);
+
+            return Task.FromResult(streamEventsPage);
         }
 
         public void Initialize()
@@ -46,7 +106,7 @@
             var connection = _getConnection();
             connection.CreateTable<Event>();
             connection.CreateIndex("Events", "EventId", true);
-            connection.CreateIndex("Events", new []{ "BucketId", "StreamId", "Sequence"} , true);
+            connection.CreateIndex("Events", new []{ "BucketId", "StreamId", "SequenceNumber"} , true);
         }
 
         public void Drop()
@@ -57,7 +117,6 @@
 
         public void Dispose()
         {}
-
 
         [Table("Events")]
         private class Event
@@ -71,7 +130,7 @@
             [NotNull]
             public Guid EventId { get; set; }
 
-            public int Sequence { get; set; }
+            public int SequenceNumber { get; set; }
 
             [PrimaryKey, AutoIncrement]
             public int Checkpoint { get; set; }
@@ -83,12 +142,132 @@
             public bool IsDeleted { get; set; }
 
             [NotNull]
-            public DateTime Stamp { get; set; }
+            public DateTimeOffset Stamp { get; set; }
 
             public byte[] Headers { get; set; }
 
-             [NotNull]
+            [NotNull]
             public byte[] Body { get; set; }
+        }
+
+        private class StreamEventsPage : IStreamEventsPage
+        {
+            private readonly int _fromSequenceNumber;
+            private readonly bool _isEndOfStream;
+            private readonly int _lastSequenceNumber;
+            private readonly int _nextSequenceNumber;
+            private readonly ReadDirection _readDirection;
+            private readonly PageReadStatus _status;
+            private readonly string _streamId;
+            private readonly IReadOnlyCollection<IStreamEvent> _events; 
+
+            public StreamEventsPage(
+                string streamId,
+                PageReadStatus pageReadStatus,
+                ReadDirection readDirection,
+                int fromSequenceNumber,
+                int lastSequenceNumber,
+                int nextSequenceNumber,
+                bool isEndOfStream,
+                Event[] events)
+            {
+                _streamId = streamId;
+                _status = pageReadStatus;
+                _readDirection = readDirection;
+                _fromSequenceNumber = fromSequenceNumber;
+                _lastSequenceNumber = lastSequenceNumber;
+                _nextSequenceNumber = nextSequenceNumber;
+                _isEndOfStream = isEndOfStream;
+
+                var streamEvents = events
+                    .Select(e => new StreamEvents(e))
+                    .Cast<IStreamEvent>()
+                    .ToList();
+
+                _events = new ReadOnlyCollection<IStreamEvent>(streamEvents);
+            }
+
+            public IReadOnlyCollection<IStreamEvent> Events
+            {
+                get { return _events; }
+            }
+
+            public int FromSequenceNumber
+            {
+                get { return _fromSequenceNumber; }
+            }
+
+            public bool IsEndOfStream
+            {
+                get { return _isEndOfStream; }
+            }
+
+            public int LastSequenceNumber
+            {
+                get { return _lastSequenceNumber; }
+            }
+
+            public int NextSequenceNumber
+            {
+                get { return _nextSequenceNumber; }
+            }
+
+            public ReadDirection ReadDirection
+            {
+                get { return _readDirection; }
+            }
+
+            public PageReadStatus Status
+            {
+                get { return _status; }
+            }
+
+            public string StreamId
+            {
+                get { return _streamId; }
+            }
+
+            private class StreamEvents : IStreamEvent
+            {
+                private readonly byte[] _body;
+                private readonly Guid _eventId;
+                private readonly IReadOnlyDictionary<string, string> _headers;
+                private readonly int _sequenceNumber;
+                private readonly string _streamId;
+
+                public StreamEvents(Event @event)
+                {
+                    _eventId = @event.EventId;
+                    _body = @event.Body;
+                    _sequenceNumber = @event.SequenceNumber;
+                    _streamId = @event.StreamId;
+                }
+
+                public byte[] Body
+                {
+                    get { return _body; }
+                }
+
+                public Guid EventId
+                {
+                    get { return _eventId; }
+                }
+
+                public IReadOnlyDictionary<string, string> Headers
+                {
+                    get { return _headers; }
+                }
+
+                public int SequenceNumber
+                {
+                    get { return _sequenceNumber; }
+                }
+
+                public string StreamId
+                {
+                    get { return _streamId; }
+                }
+            }
         }
     }
 }
